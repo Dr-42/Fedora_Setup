ebf5829669e17d7fc1155991903290b6d8bdad4a
Spandan Roy <Spandan Roy>
2023-04-22 20:51:46 +0530

    SDDM over GDM

--- fed_kick_root.sh
+++ fed_kick_root.sh
@@ -14,6 +14,9 @@ dnf install xorg-x11-drv-nvidia-libs -y
 
 # Replace GNOME with KDE
 dnf swap @gnome-desktop @kde-desktop -y
+dnf install sddm
+systemctl disable gdm
+systemctl enable sddm
 
 # Install Important packages
 dnf install alacriitty -y
@@ -43,3 +46,6 @@ dnf install nodejs -y
 
 #install filelight
 dnf install filelight -y
+
+#install htop
+dnf install htop -y
